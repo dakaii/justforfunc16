27c6cf4f9d3d57938165f6a3a8c12d54aa060f40
dakaii <daiki815@gmail.com>
2018-05-16 09:49:13 +0800

    13:03

--- sum_test.go
+++ sum_test.go
@@ -4,18 +4,19 @@ import "testing"
 
 func TestInts(t *testing.T) {
 	tt := []struct {
+		name    string
 		numbers []int
 		sum     int
 	}{
-		{[]int{1, 2, 3, 4, 5}, 15},
-		{nil, 0},
-		{[]int{1, -1}, 0},
+		{"one to five", []int{1, 2, 3, 4, 5}, 15},
+		{"no numbers", nil, 0},
+		{"one and minus one", []int{1, -1}, 0},
 	}
 
 	for _, tc := range tt {
 		s := Ints(tc.numbers...)
 		if s != tc.sum {
-			t.Errorf("sum of %v should be %v; got %v", tc.numbers, tc.sum, s)
+			t.Errorf("sum of %v should be %v; got %v", tc.name, tc.sum, s)
 		}
 	}
 }
